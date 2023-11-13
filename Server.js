4f013bb80ca836e5232b5d106f74efc97299cd16
Pratik Sawarn <pandeyshiv006@gmail.com>
2023-11-13 18:43:39 +0530

    REST API basic

---
+++ Server.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+
+const express=require("express")
+const mongoose=require("mongoose")
+const subscriberRouter=require('./routes/subscriber')
+
+mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser: true})
+
+const db=mongoose.connection
+db.on('error',(error)=>console.error(error))
+db.once('open',()=>{console.log("Database Connected :)")})
+
+const app=express();
+
+app.use(express.json())
+
+app.use('/subscriber',subscriberRouter)
+
+
+app.listen(3000,()=>
+    console.log("server Started")
+)
